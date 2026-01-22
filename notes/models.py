ade1994a08964faa048e9af03ae9293a14e464ee
Ivailo Iliqnov Iliev <ivailoiliev89@gmail.com>
2026-01-22 10:26:48 +0200

    Update models.py

--- notes/models.py
+++ notes/models.py
@@ -2,13 +2,22 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    color = models.CharField(max_length=20, default='secondary')
+
+    def __str__(self):
+        return self.name
+
+
 class Note(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notes')
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
+    tags = models.ManyToManyField(Tag, blank=True, related_name='tags')
     created_at = models.DateTimeField(auto_now_add=True)
 
-    # show the newone first
+    # show the new one first
     class Meta:
         ordering = ['-created_at']
 
