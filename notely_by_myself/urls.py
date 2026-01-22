655dbef70228169d207588e2ecc8b32ad1cb2075
Ivailo Iliqnov Iliev <ivailoiliev89@gmail.com>
2026-01-22 10:25:09 +0200

    Update URL patterns for notes API

--- notely_by_myself/urls.py
+++ notely_by_myself/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for notely_by_myself project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
@@ -21,13 +5,14 @@ from rest_framework.routers import DefaultRouter
 from notes.views import NoteViewSet
 
 router = DefaultRouter()
-router.register(r'notes', NoteViewSet, basename='note')
+router.register(r'notes-api', NoteViewSet, basename='note-api')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
-    path('accounts/', include('accounts.urls')),  # our registrations are here
+    path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),  # Login/Logout/Password
-    path('notes', include('notes.urls')),  # CRUD
+    path('notes/', include('notes.urls')),  # CRUD
     path('api/', include(router.urls)),
+    # path('tags/', include('tags.urls')),  # tasks
 ]
