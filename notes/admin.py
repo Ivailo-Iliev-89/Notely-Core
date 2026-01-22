8d7259981e3cd9423283fe7aa04bc82c23866f71
Ivailo Iliqnov Iliev <ivailoiliev89@gmail.com>
2026-01-22 10:25:52 +0200

    Update admin.py

--- notes/admin.py
+++ notes/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import Note
+from .models import Note, Tag
 
 
 @admin.register(Note)
 class NoteAdmin(admin.ModelAdmin):
     list_display = ('title', 'owner', 'created_at')
     search_fields = ('title', 'owner', 'owner__username')
+
+
+admin.site.register(Tag)
